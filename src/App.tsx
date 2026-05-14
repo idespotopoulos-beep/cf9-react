080348e66d41b65f3b5691f26e3ff55805438062
Ioannis Despotopoulos <i.despotopoulos@gmail.com>
2026-05-14 20:58:10 +0300

    feat: add CounterAdvanced component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@
 // import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
 import Layout from "./components/Layout.tsx";
-import Counter from "./components/LessonFour/Counter.tsx";
+import CounterAdvanced from "./components/LessonFour/CounterAdvanced.tsx";
+// import Counter from "./components/LessonFour/Counter.tsx";
+// import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
+// import {useState} from "react";
 
 // type PropsA = {
 //   title: string,
@@ -69,7 +72,9 @@ function App() {
 
             {/*LESSON FOUR*/}
             <Layout addClasses="bg-gray-50">
-                <Counter/>
+                {/*<Counter/>*/}
+                {/*<ClassCounter/>*/}
+                <CounterAdvanced/>
             </Layout>
         </>
     )
